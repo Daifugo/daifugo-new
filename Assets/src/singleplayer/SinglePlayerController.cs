e3b4e2e11d64b6425bdf9d3e75b5738063dab654
Adrian <adrianasdfs@gmail.com>
2017-01-29 16:14:07 +0800

    add controller file for hte Single Player scene

---
+++ Assets/src/singleplayer/SinglePlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinglePlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/* Button Handlers */
+
+	public void back()
+	{
+		SceneManager.LoadScene ("main");
+	}
+
+	public void next()
+	{
+
+	}
+
+
+}
