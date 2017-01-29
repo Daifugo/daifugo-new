79faee4ab625cb985f4bc984230497b2481377f6
Adrian <adrianasdfs@gmail.com>
2017-01-30 00:53:02 +0800

    add ruleId property, and getters and setters

--- Assets/src/Util/RuleItem.cs
+++ Assets/src/Util/RuleItem.cs
@@ -9,20 +9,27 @@ public class RuleItem : MonoBehaviour {
 	public GameObject ruleDescription;
 	public GameObject ruleCheckMark;
 
+	private string _ruleId;
 	private float _height = 0.0f;
 	private float _YCoor = 0.0f;
 
 
-	public void setRuleDetails(string name, string description)
+	public void setRuleDetails(string name, string description, string ruleId)
 	{
 		ruleName.GetComponent<Text>().text = name;
 		ruleDescription.GetComponent<Text>().text = description;
+		_ruleId = ruleId;
 	}
 
 	public void setGeometry(float YCoor, float height)
 	{
 		_height = height;
 		_YCoor = YCoor;
+	}
+
+	public string getRuleId()
+	{
+		return _ruleId;
 	}
 
 	public void setToggleUIHandler(UnityEngine.Events.UnityAction<bool> handler)
