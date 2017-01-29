83b90f0ffc4d4f6b4ce3bb1c49877d408bc7acfe
Adrian <adrianasdfs@gmail.com>
2017-01-29 23:43:43 +0800

    Script component for each rule item in the list

---
+++ Assets/src/Util/RuleItem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RuleItem : MonoBehaviour {
+ 
+
+	public GameObject ruleName;
+	public GameObject ruleDescription;
+
+	private float _height = 0.0f;
+	private float _YCoor = 0.0f;
+
+
+	public void setRuleDetails(string name, string description)
+	{
+		ruleName.GetComponent<Text>().text = name;
+		ruleDescription.GetComponent<Text>().text = description;
+	}
+
+	public void setGeometry(float YCoor, float height)
+	{
+		_height = height;
+		_YCoor = YCoor;
+	}
+
+	// Use this for initialization
+
+	void Start () {
+
+		gameObject.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+		gameObject.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+		gameObject.GetComponent<RectTransform> ().sizeDelta = new Vector2(0.0f,_height);
+		gameObject.GetComponent<RectTransform>().anchoredPosition= new Vector2(0.0f,_YCoor);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
