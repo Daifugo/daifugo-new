1aeb7a8e28ba77079d5f8e9bc6dae435caefeffe
Adrian <adrianasdfs@gmail.com>
2017-01-29 21:41:30 +0800

    add Transporter object & request Rules

--- Assets/src/singleplayer/SinglePlayerController.cs
+++ Assets/src/singleplayer/SinglePlayerController.cs
@@ -1,16 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class SinglePlayerController : MonoBehaviour {
+public class SinglePlayerController : MonoBehaviour, SocketConnectionInterface {
+
+	public GameObject transporter;
+	private Transporter _tr;
 
 	// Use this for initialization
 	void Start () {
-	
+
+		_tr = transporter.GetComponent<Transporter>();
+		_tr.setSocketDelegate(this);
+
+		_tr.requestRules();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	/* SocketConnectionInterface */
+
+	public void receiveData(string dt)
+	{
+
+	}
+
+	public void handleError()
+	{
+
 	}
 
 	/* Button Handlers */
