3573666d763ae4b1b98eb1108b511837a3517793
Adrian <adrianasdfs@gmail.com>
2017-01-30 01:03:33 +0800

    add handler to the toggle check mark

--- Assets/src/singleplayer/RuleListContainer.cs
+++ Assets/src/singleplayer/RuleListContainer.cs
@@ -9,6 +9,25 @@ public class RuleListContainer : MonoBehaviour {
 	private int ruleCount = 0;
 	private float ruleItemGap = 5.0f;
 
+	private List<string> selectedRules;
+
+
+	public void toggleHandler(GameObject s)
+	{
+		bool isToggleActive = s.GetComponent<RuleItem> ().isToggleActive();
+
+		if(isToggleActive)
+		{
+			selectedRules.Add(s.GetComponent<RuleItem> ().getRuleId());
+		}
+		else
+		{
+			selectedRules.Remove(s.GetComponent<RuleItem> ().getRuleId());
+		}
+
+	}
+
+
 
 	public void addRule(Dictionary<string,string> rule)
 	{
@@ -18,20 +37,33 @@ public class RuleListContainer : MonoBehaviour {
 
 		string ruleName = null;
 		string ruleDescription = null;
+		string ruleId = null;
 
 		rule.TryGetValue ("ruleName", out ruleName);
 		rule.TryGetValue ("ruleDescription", out ruleDescription);
+		// rule.TryGetValue("ruleId", out ruleId);
 
 		/* set details */
 
-		ruleObj.GetComponent<RuleItem> ().setRuleDetails (ruleName, ruleDescription);
+		ruleObj.GetComponent<RuleItem> ().setRuleDetails (ruleName, ruleDescription,ruleId);
 
 		/* set geometry */
 
 		float YCoor = (((ruleCount * ruleItemHeight) + (ruleCount * ruleItemGap)) * (-1)) + startRuleYCoor;
 		ruleObj.GetComponent<RuleItem> ().setGeometry(YCoor, ruleItemHeight);
 
+		/* add handler */
+
+		ruleObj.GetComponent<RuleItem> ().setToggleUIHandler(delegate{
+			toggleHandler(ruleObj);
+		});
+
 		ruleCount++;
+	}
+
+	void Start()
+	{
+		selectedRules = new List<string>();
 	}
 
 }
