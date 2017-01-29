b2fd77728831a6a8301cc118aaa04a2eef991eb2
Adrian <adrianasdfs@gmail.com>
2017-01-29 20:57:36 +0800

    Transporter object: to interact with the SocketConnection object

---
+++ Assets/src/Util/Transporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transporter : MonoBehaviour {
+
+	public GameObject sockConn;
+
+	private SocketConnection _socket = null;
+
+	void Awake()
+	{
+		_socket = sockConn.GetComponent<SocketConnection> ();
+
+		DontDestroyOnLoad (sockConn);
+	}
+}
