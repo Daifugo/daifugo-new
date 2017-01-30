30c7990c0040b27e26cbb404ae69515fd9e0c9c9
Adrian <adrianasdfs@gmail.com>
2017-01-30 20:09:24 +0800

    script component to manipulate the room list

---
+++ Assets/src/multiplayer/RoomListContainer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomListContainer : MonoBehaviour {
+
+
+	public void addRooms(Dictionary<string,object> rule)
+	{
+
+		GameObject roomPrefab = Resources.Load ("prefabs/room", typeof(GameObject)) as GameObject;
+		GameObject roomObj = Instantiate (roomPrefab, Vector3.zero, Quaternion.identity, transform) as GameObject;
+
+		object roomName = null;
+		object rules = null;
+		object numOfPlayer = null;
+
+		rule.TryGetValue ("roomName", out roomName);
+		rule.TryGetValue ("rules", out rules);
+		rule.TryGetValue("numOfPlayer", out numOfPlayer);
+
+
+		RoomItem r = roomObj.GetComponent<RoomItem> ();
+
+		/* set details */
+
+		r.setRoomDetails((string[])rules,(string)roomName,(string)numOfPlayer);
+	}
+
+	void Start()
+	{
+
+	}
+
+}
