f0c9e9184d81a419e93b636ed136b7745e271ca5
Adrian <adrianasdfs@gmail.com>
2017-01-30 22:36:17 +0800

    script for the Avatar scene controller

---
+++ Assets/src/avatar/AvatarSceneController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarSceneController : MonoBehaviour,SocketConnectionInterface {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void receiveData(string dt)
+	{
+
+	}
+
+	public void handleError()
+	{
+
+	}
+
+}
