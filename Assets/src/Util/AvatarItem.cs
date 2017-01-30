e6e3db8654d6d5d0ec982ecc1aef689c0d12f409
Adrian <adrianasdfs@gmail.com>
2017-01-30 23:12:45 +0800

    script component for each avatar UI thumbnail

---
+++ Assets/src/Util/AvatarItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarItem : MonoBehaviour {
+
+
+	public GameObject overlay;
+	private bool _selected = false;
+
+	public void toggleShowSelected()
+	{
+		_selected = !_selected;
+		overlay.SetActive(_selected);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
