3baf8a2ea4c3786bcd8a8b3c153a47c89c1a9d50
Adrian <adrianasdfs@gmail.com>
2017-01-30 16:46:08 +0800

    script file for the Multiplayer scene controller

---
+++ Assets/src/multiplayer/MultiplayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+public class MultiplayerController : MonoBehaviour, SocketConnectionInterface {
+
+	public GameObject transporter;
+	public RuleListContainer ruleList;
+
+	private Transporter _tr;
+
+	// Use this for initialization
+	void Start () {
+
+		_tr = transporter.GetComponent<Transporter>();
+		_tr.setSocketDelegate(this);
+
+		_tr.requestRooms();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	/* SocketConnectionInterface */
+
+	public void receiveData(string dt)
+	{
+
+	}
+
+	public void handleError()
+	{
+
+	}
+
+
+}
