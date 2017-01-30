267a7027f34460466f0221615d2ed46f8f58824c
Adrian <adrianasdfs@gmail.com>
2017-01-30 16:03:20 +0800

    script file for the avatar scene controller

---
+++ Assets/src/avatar/AvatarController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AvatarController : MonoBehaviour {
+
+
+	private Transform _selectedAvatar = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	/* Animation Event Callback */
+
+	public void addClickHandlers()
+	{
+
+		avatarThumbnailHandler(transform.GetChild(0));
+
+		for(int i = 0;i < transform.childCount;i++)
+		{
+			Transform child = transform.GetChild(i);
+
+			child.GetComponent<Button>().onClick.AddListener(delegate{
+
+				avatarThumbnailHandler(child);
+
+			});
+		}
+	}
+	
+	private void avatarThumbnailHandler(Transform s)
+	{
+
+		if(_selectedAvatar != null)
+			a(_selectedAvatar,false);
+
+		_selectedAvatar = s;
+		a(s,true);
+	}
+
+
+	private void a(Transform c, bool status)
+	{
+
+		for(int i = 0;i < c.childCount;i++)
+		{
+			Transform child = c.GetChild(i);
+
+			if(child.tag == "avatarOverlay")
+				child.gameObject.SetActive(status);
+		}
+
+	}
+
+}
