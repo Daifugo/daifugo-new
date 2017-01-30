a34dae00ac24c1852767ba9ca8578ea4cd152a78
Adrian <adrianasdfs@gmail.com>
2017-01-30 20:32:30 +0800

    change the item's geometry in the list

--- Assets/src/Util/RoomItem.cs
+++ Assets/src/Util/RoomItem.cs
@@ -8,6 +8,9 @@ public class RoomItem : MonoBehaviour {
 	public GameObject rules;
 	public GameObject numOfPlayers;
 
+	private float _height = 0.0f;
+	private float _YCoor = 0.0f;
+
 
 	public void setRoomDetails(string[] rulesStr, string name, string num)
 	{
@@ -38,12 +41,21 @@ public class RoomItem : MonoBehaviour {
 
 	}
 
-
+	public void setGeometry(float YCoor, float height)
+	{
+		_height = height;
+		_YCoor = YCoor;
+	}
 
 
 	// Use this for initialization
 	void Start () {
 	
+		gameObject.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+		gameObject.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+		gameObject.GetComponent<RectTransform> ().sizeDelta = new Vector2(0.0f,_height);
+		gameObject.GetComponent<RectTransform>().anchoredPosition= new Vector2(0.0f,_YCoor);
+
 	}
 	
 	// Update is called once per frame
