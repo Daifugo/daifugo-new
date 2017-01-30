de8effc3d503f233fe8ca8c98760b7dbd8adf2f2
Adrian <adrianasdfs@gmail.com>
2017-01-30 22:52:55 +0800

    request selected avatar if user chose multiplayer mode

--- Assets/src/avatar/AvatarSceneController.cs
+++ Assets/src/avatar/AvatarSceneController.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AvatarSceneController : MonoBehaviour,SocketConnectionInterface {
 
 
 	// Use this for initialization
+
+	Transporter _tr = null;
+
 	void Start () {
 	
+		/* Get Transporter Object from previous scene */
+
+		_tr = GameObject.Find("Transporter").GetComponent<Transporter>();
+		_tr.setSocketDelegate(this);
+
+
+		/* If user wants multiplayer */
+
+		// if(PlayerPrefs.HasKey("roomId"))
+		// {	
+			Dictionary<string,object> data = new Dictionary<string,object>(){
+				{"roomId", "asdfs"}
+			};
+
+			_tr.requestSelectedAvatars(data);
+		// }
+
 	}
 	
 	// Update is called once per frame
