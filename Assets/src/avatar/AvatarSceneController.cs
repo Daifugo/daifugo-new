b007148632ab32e26e4e3a4fadc19e9343724714
Adrian <adrianasdfs@gmail.com>
2017-01-31 12:24:05 +0800

    use coroutine to parse incoming data

--- Assets/src/avatar/AvatarSceneController.cs
+++ Assets/src/avatar/AvatarSceneController.cs
@@ -7,10 +7,11 @@ using Newtonsoft.Json.Linq;
 public class AvatarSceneController : MonoBehaviour,SocketConnectionInterface {
 
 
-	// Use this for initialization
+	public GameObject avatarContainer;
 
 	Transporter _tr = null;
 	JToken _responseToken = null;
+
 
 	void Start () {
 	
@@ -30,12 +31,29 @@ public class AvatarSceneController : MonoBehaviour,SocketConnectionInterface {
 
 			_tr.requestSelectedAvatars(data);
 		}
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	IEnumerator parseData()
+	{
+
+		/* Only expect data with the REQUESTAVATARS_CODE */
+
+		while(_responseToken == null)
+			yield return null;
+
+		JArray rms = (JArray)_responseToken;
+		AvatarsContainer ava = avatarContainer.GetComponent<AvatarsContainer> ();
+
+		foreach (JToken s in rms) 
+		{
+			ava.disableAvatars ((int)s);
+		}
+
 	}
 
 	public void receiveData(string dt)
