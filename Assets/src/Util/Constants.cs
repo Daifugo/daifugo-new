c523917fcde0d83f393d03a4a2ee2f0f97236c0b
Adrian <adrianasdfs@gmail.com>
2017-01-31 13:11:50 +0800

    add JOINGAME_CODE and ROOMLIST_CODE

--- Assets/src/Util/Constants.cs
+++ Assets/src/Util/Constants.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public static class Constants{
 
+	public const int ROOMLIST_CODE = 104;
 	public const int RULELIST_CODE = 105;
 	public const int REQUESTAVATARS_CODE = 113;
+	public const int JOINGAME_CODE = 107;
 
 }
