658c96a530d2010a9647cb8f8481e8a226317dad
Adrian <adrianasdfs@gmail.com>
2017-01-31 13:50:12 +0800

    add LOBBYDETAILS_CODE

--- Assets/src/Util/Constants.cs
+++ Assets/src/Util/Constants.cs
@@ -5,6 +5,7 @@ public static class Constants{
 
 	public const int ROOMLIST_CODE = 104;
 	public const int RULELIST_CODE = 105;
+	public const int LOBBYDETAILS_CODE = 106;
 	public const int REQUESTAVATARS_CODE = 113;
 	public const int JOINGAME_CODE = 107;
 	public const int STARTGAME_CODE = 114;
