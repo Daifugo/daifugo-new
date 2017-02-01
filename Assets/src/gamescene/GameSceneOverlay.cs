56c69cae17be3b50ccb72ecb985f857d349eff60
Adrian <adrianasdfs@gmail.com>
2017-02-02 01:00:15 +0800

    script component for the overlay in the game scene

---
+++ Assets/src/gamescene/GameSceneOverlay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameSceneOverlay : MonoBehaviour {
+
+	public GameObject stateText;
+	Text textComponent = null;
+
+	public void changeText(int state = -1)
+	{
+		switch(state)
+		{
+			case -1:
+				textComponent.text = "Loading...";
+			break;
+
+			case 1:
+				textComponent.text = "Deploying CPU Bot..";
+			break;
+		}
+	}
+
+	void Start()
+	{
+		textComponent = stateText.GetComponent<Text>();
+	}
+
+
+}
