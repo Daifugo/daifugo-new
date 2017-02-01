cd94650ea97a1b9370d9bd83687eee4731257b89
Adrian <adrianasdfs@gmail.com>
2017-02-02 00:35:02 +0800

    script component for each player in the game table

---
+++ Assets/src/Util/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public GameObject avatar;
+	public GameObject cardsCount;
+	public GameObject parent;
+
+
+	bool _isOccupied = false;
+	string _userId = null;
+
+
+	public bool isSpaceVacant()
+	{
+		return (_isOccupied == false);
+	}
+
+
+	public void setPlayer(string userId, int photoId)
+	{
+		setAvatar(photoId);
+		_userId = userId;
+		_isOccupied = true;
+	}
+
+
+	private void setAvatar(int photoId)
+	{
+		GameObject ava = Resources.Load ("prefabs/gameAvatar"+(photoId+1), typeof(GameObject)) as GameObject;
+		GameObject s = (GameObject)Instantiate(ava,Vector3.zero,Quaternion.identity,avatar.transform);
+		s.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+		s.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+		s.GetComponent<RectTransform>().sizeDelta = new Vector2(150.0f,150.0f);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
