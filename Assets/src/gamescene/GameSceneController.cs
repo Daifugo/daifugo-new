cd7ed3744e829102ace8835d2cfa9752b44dd550
Adrian <adrianasdfs@gmail.com>
2017-02-02 01:01:58 +0800

    add a property for the overlay

--- Assets/src/gamescene/GameSceneController.cs
+++ Assets/src/gamescene/GameSceneController.cs
@@ -5,17 +5,20 @@ public class GameSceneController : MonoBehaviour {
 
 	GameObject[] playerSpaces = null;
 
+	public GameObject overlay;
+
 	void Start () {
 		
 		playerSpaces = GameObject.FindGameObjectsWithTag("player");
 
+		overlay.SetActive(true);
+		overlay.GetComponent<GameSceneOverlay>().changeText();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
 
 	public void addPlayer(string userid, int photoId)
 	{
