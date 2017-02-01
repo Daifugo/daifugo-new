0f427565cf0c357881c953f8b7cd0e11cd0c66b9
Adrian <adrianasdfs@gmail.com>
2017-02-01 21:54:23 +0800

    script file for game scene controller

---
+++ Assets/src/gamescene/GameSceneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSceneController : MonoBehaviour {
+
+	GameObject[] playerSpaces = null;
+
+	void Start () {
+		
+		playerSpaces = GameObject.FindGameObjectsWithTag("player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	
+}
