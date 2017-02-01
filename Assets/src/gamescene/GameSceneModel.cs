698849a914f1748063f9f27ff3ffdf4640479675
Adrian <adrianasdfs@gmail.com>
2017-02-01 21:21:48 +0800

    scripts for the game scene

---
+++ Assets/src/gamescene/GameSceneModel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSceneModel : MonoBehaviour,SocketConnectionInterface {
+
+
+	Transporter _transporter = null;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		_transporter = GameObject.Find("Transporter").GetComponent<Transporter>();
+		_transporter.setSocketDelegate (this);
+
+	}
+
+
+	public void receiveData(string dt)
+	{
+
+	}
+
+
+	public void handleError()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
