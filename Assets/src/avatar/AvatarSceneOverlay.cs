d3f287409ad0485f3fa9d262829a0cb5745d9761
Adrian <adrianasdfs@gmail.com>
2017-02-02 11:31:36 +0800

    script component for the Avatar scene overlay

---
+++ Assets/src/avatar/AvatarSceneOverlay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AvatarSceneOverlay : MonoBehaviour {
+
+	public GameObject textOBject; 
+
+	public void showJoin()
+	{
+		textOBject.GetComponent<Text>().text = "Joining Room...";
+	}
+
+	public void showStart()
+	{
+		textOBject.GetComponent<Text>().text = "Creating A Room...";
+	}
+
+	public void show()
+	{
+		gameObject.GetComponent<Image>().enabled = true;
+	}
+
+}
