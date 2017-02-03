fc2e1f713abbe20650cffcdeffea77b71b34f721
Adrian <adrianasdfs@gmail.com>
2017-02-04 02:54:02 +0800

    display dealt card

--- Assets/src/Util/MainUserCardRenderer.cs
+++ Assets/src/Util/MainUserCardRenderer.cs
@@ -11,18 +11,91 @@ public class MainUserCardRenderer : MonoBehaviour {
 	private const float _cardWidth = 153.0f;
 	private const float _cardHeight = 220.0f;
 
+	private const float _dealtCardWidth = 70.0f;
+	private const float _dealtCardHeight = 100.0f;
+	private const float _DEALTCARD_SPACE = 30.0f;
+	private const float _dealtCardY = 0.0f;
 
-	public GameObject renderCard(int suit, int rank)
+
+	public GameObject renderCard(Card s)
 	{
 		GameObject card = Resources.Load("prefabs/GameCard", typeof(GameObject)) as GameObject;
 		card = Instantiate (card, Vector3.zero, Quaternion.identity, transform) as GameObject;
 
-		setImage(card, suit, rank);
+		card.GetComponent<GameCard>().addCard(s);
+
+		setImage(card, s.getSuit(), s.getRank());
 		setGeometry(card);
 
 		return card;
 	}
 
+
+	public void renderDealt(Card[] s)
+	{
+		StartCoroutine(displayDealt(s));
+	}
+
+
+	IEnumerator displayDealt(Card[] cards)
+	{
+		float xCoor = calculateStartX(cards.Length);
+
+		yield return new WaitForSeconds (1.3f);
+
+		foreach(var card in cards)
+		{
+
+			GameObject cardPrefab = Resources.Load("prefabs/maincard", typeof(GameObject)) as GameObject;
+			cardPrefab = Instantiate (cardPrefab, Vector3.zero, Quaternion.identity, transform) as GameObject;
+
+			setDealtImage(cardPrefab, card.getSuit(),card.getRank());
+			setDealtGeometry(cardPrefab, xCoor);
+
+			xCoor += _DEALTCARD_SPACE;
+
+			yield return new WaitForSeconds (0.9f);
+		}
+		
+	}
+
+
+	float calculateStartX(int length)
+	{
+		int numOfLeftSideCards = length / 2;
+		float xCoor = 0.0f;
+
+		if (length % 2 != 0) {
+			
+			xCoor = numOfLeftSideCards * _DEALTCARD_SPACE * -1;
+
+		} else {
+
+			float m = ((_dealtCardWidth / 2) / 2);
+			float h = (numOfLeftSideCards - 1) * _DEALTCARD_SPACE;
+			xCoor = (h + m) * -1;
+
+		}
+
+		return xCoor;
+	}
+
+	void setDealtImage(GameObject card, int suit, int rank)
+	{
+		card.GetComponent<Image>().sprite = _spawner.getSprite (suit, rank);
+	}
+
+	void setDealtGeometry(GameObject card, float xCoor)
+	{
+		RectTransform rect = card.GetComponent<RectTransform>();
+
+		rect.sizeDelta = new Vector2(_dealtCardWidth,_dealtCardHeight);
+		rect.anchoredPosition= new Vector2(xCoor,_dealtCardY);
+
+		rect.anchorMax = new Vector2(0.5f,0.5f);
+		rect.anchorMin = new Vector2(0.5f,0.5f);
+
+	}
 
 	void setImage(GameObject card, int suit, int rank)
 	{
