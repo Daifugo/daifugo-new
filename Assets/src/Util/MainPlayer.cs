3e0fcadb20a5f8aa1f31c2082150ea2ef9ed0b5d
Adrian <adrianasdfs@gmail.com>
2017-02-03 20:32:24 +0800

    script component for the main player gameobject

---
+++ Assets/src/Util/MainPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainPlayer : MonoBehaviour {
+
+	public GameObject cardLocation;
+	public GameObject actions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void buttonCardHandler(GameObject c)
+	{
+		c.GetComponent<GameCard>().selected();
+	}
+
+	public void addCard(Card s)
+	{
+		GameObject c = cardLocation.GetComponent<MainUserCardRenderer>().renderCard(s.getSuit(),s.getRank());
+		c.GetComponent<GameCard>().addHandler(delegate{
+			buttonCardHandler(c);
+		});
+	}
+
+}
