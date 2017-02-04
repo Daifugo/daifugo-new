28f0ad2e74c97d6eb2090d0b9b0538b2cc1cee64
Adrian <adrianasdfs@gmail.com>
2017-02-05 00:07:07 +0800

    base class of DealtCardRenderer & OwnedCardRenderer

---
+++ Assets/src/Util/CardRenderer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public abstract class CardRenderer : MonoBehaviour {
+
+	protected CardGameSpawner _spawner = null;
+
+	public virtual GameObject render(Card s){ return null; }
+	public virtual void render(Card[] s){}
+
+	public abstract void removeCard(Card s); 
+
+	protected abstract void setImage(GameObject card, int suit, int rank);
+	protected abstract void setGeometry(GameObject card, float xCoor = 0.0f);
+
+	protected virtual GameObject getObject(string prefabName)
+	{
+		GameObject cardPrefab = Resources.Load("prefabs/"+prefabName, typeof(GameObject)) as GameObject;
+		return Instantiate (cardPrefab, Vector3.zero, Quaternion.identity, transform) as GameObject;
+	}
+	
+}
