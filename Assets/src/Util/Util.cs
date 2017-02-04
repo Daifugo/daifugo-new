189c7dc60daab0655a9027a7391e38daf88f93e4
Adrian <adrianasdfs@gmail.com>
2017-02-05 00:02:35 +0800

    Utility class

---
+++ Assets/src/Util/Util.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util{
+
+	public static Sprite getSprite(string path)
+	{
+		Texture2D tex = Resources.Load ("images/"+path, typeof(Texture2D)) as Texture2D;
+		
+		var rect = new Rect (0, 0,tex.width, tex.height);
+		Sprite x = Sprite.Create (tex, rect, Vector2.zero);
+
+		return x;
+	}
+
+}
