f956808613193e806a6f07e791021ad62cd603b6
Adrian <adrianasdfs@gmail.com>
2017-02-09 17:10:03 +0800

    dealt card renderer for the left hand side player

---
+++ Assets/src/Util/PlayerLeftCardRenderer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLeftCardRenderer : PlayerRightCardRenderer {
+
+	// Use this for initialization
+	void Start () {
+	_spawner = new CardGameSpawner();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected override float calculateX(int length)
+	{
+		if(length == 1)
+		{
+			return 100.0f;
+		}
+		else if(length < 5)
+		{
+			float u = (pivotX2Coor / length);
+			float x = length == 2?u-5.0f:u;
+			return (x + 20);
+		}
+		else
+		{
+			var h = (pivotX2Coor / length) + 15;
+			return h;
+		}
+	}
+
+
+}
