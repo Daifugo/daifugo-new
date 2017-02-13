ba0307805739afeb6d0054c24465f5de7b02d1be
Adrian <adrianasdfs@gmail.com>
2017-02-13 09:52:35 +0800

    script component for the rulesgameobject in the overlay

---
+++ Assets/src/Util/RulesOverlay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RulesOverlay : MainPlayerTurnview {
+
+	public GameObject rule;
+
+	public void show(bool isPass, string rulesName)
+	{
+		rule.GetComponent<Text>().text = rulesName;
+		base.show(isPass);
+	}
+
+}
