9e85ff3190540b787e5f2039fbe8428c4e6df20f
Adrian <adrianasdfs@gmail.com>
2017-02-25 14:13:32 +0800

    remove all receiving data logic

--- Assets/src/avatar/AvatarSceneController.cs
+++ Assets/src/avatar/AvatarSceneController.cs
@@ -3,9 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-using Newtonsoft.Json.Linq;
 
-public class AvatarSceneController : MonoBehaviour,SocketConnectionInterface {
+public class AvatarSceneController : MonoBehaviour{
 
 
 	public GameObject avatarContainer;
@@ -13,85 +12,28 @@ public class AvatarSceneController : MonoBehaviour,SocketConnectionInterface {
 	public GameObject nextButton;
 
 	Transporter _tr = null;
-	JToken _responseToken = null;
-	int _responseCode = 0;
+
 
 	void Start () {
 	
 		/* Get Transporter Object from previous scene */
 
+		DontDestroyOnLoad(GameObject.Find("Transporter"));
 		_tr = GameObject.Find("Transporter").GetComponent<Transporter>();
-		_tr.setSocketDelegate(this);
 
 
 		/* If user wants multiplayer */
 
 		if(PlayerPrefs.HasKey("roomId"))
-		{	
+		{
 			Dictionary<string,object> data = new Dictionary<string,object>(){
 				{"roomId", "asdfs"}
 			};
 
 			_tr.requestSelectedAvatars(data);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-
-	IEnumerator parseData()
-	{
-
-		while(true)
-		{
-			while(_responseToken == null)
-				yield return null;
-
-
-			switch(_responseCode)
-			{
-				case Constants.LOBBYDETAILS_CODE:
-
-					JObject userObject = (JObject)_responseToken;
-					string tempId = (string)userObject.GetValue("userId");
-					PlayerPrefs.SetString ("userId", tempId);
-					SceneManager.LoadScene ("game");
-
-				break;
-
-				case Constants.REQUESTAVATARS_CODE:
-
-					JArray rms = (JArray)_responseToken;
-					AvatarsContainer ava = avatarContainer.GetComponent<AvatarsContainer> ();
-
-					foreach (JToken s in rms) 
-						ava.disableAvatars ((int)s);
-
-				break;
-			}
-
-			_responseToken = null;
-		}
-	}
-
-
-	public void receiveData(string dt)
-	{
-		JArray resArray = JArray.Parse (dt);
-		JToken response = resArray.First["response"];
-
-		_responseToken = response.SelectToken ("data");
-		_responseCode = (int)response.SelectToken("code");
-	}
-
-	public void handleError()
-	{
 
 	}
-
 
 	public void showNextButton()
 	{
