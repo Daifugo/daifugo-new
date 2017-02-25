d1978604e3320b4a3f58a77b175c4397a6fd9c78
Adrian <adrianasdfs@gmail.com>
2017-02-25 12:31:53 +0800

    Script component for the overlay in mainmenu

---
+++ Assets/src/main-menu/MainMenuOverlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuOverlay : MonoBehaviour {
+
+	public GameObject messageBox;
+
+	public void showMessage()
+	{
+		gameObject.SetActive(true);
+		messageBox.GetComponent<Animator>().SetBool("show", true);
+	}
+}
