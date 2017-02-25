da68a39e3dbc26e409978b74d45c07f09d63471d
Adrian <adrianasdfs@gmail.com>
2017-02-25 14:32:54 +0800

    Model component of the avatar scene

---
+++ Assets/src/avatar/AvatarSceneModel.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+
+public class AvatarSceneModel : MonoBehaviour, SocketConnectionInterface {
+
+	public GameObject controller;
+	public GameObject transporter;
+	
+	JToken _responseToken = null;
+	int _responseCode = 0;
+	Transporter _tr = null;
+	AvatarSceneController _controller;
+
+	void Start () {
+		
+		StartCoroutine(parseData());
+		_tr = transporter.GetComponent<Transporter>();
+		_tr.setSocketDelegate(this);
+		
+		_controller = controller.GetComponent<AvatarSceneController>();
+	}
+
+	
+	IEnumerator parseData()
+	{
+
+		while(true)
+		{
+			while(_responseToken == null)
+				yield return null;
+
+
+			switch(_responseCode)
+			{
+				case Constants.LOBBYDETAILS_CODE:
+				
+					JObject userObject = (JObject)_responseToken;
+					string tempId = (string)userObject.GetValue("userId");
+					_controller.receiveUserId(tempId);
+
+				break;
+
+				case Constants.REQUESTAVATARS_CODE:
+
+					JArray rms = (JArray)_responseToken;
+
+					foreach (JToken s in rms)
+						_controller.disableAvatar((int)s);
+
+				break;
+			}
+
+			_responseToken = null;
+		}
+	}
+	
+	public void receiveData(string dt)
+	{
+		JArray resArray = JArray.Parse (dt);
+		JToken response = resArray.First["response"];
+
+		_responseToken = response.SelectToken ("data");
+		_responseCode = (int)response.SelectToken("code");
+	}
+
+	public void handleError()
+	{
+		
+	}
+}
