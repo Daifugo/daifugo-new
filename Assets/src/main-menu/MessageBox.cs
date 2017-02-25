48990fae9f381b413fd4b4e76bfa042086bcee9a
Adrian <adrianasdfs@gmail.com>
2017-02-25 12:44:20 +0800

    Script component for the dialog box

---
+++ Assets/src/main-menu/MessageBox.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageBox : MonoBehaviour {
+
+	public void closeBoxHandler()
+	{
+		gameObject.SetActive(false);
+		transform.parent.gameObject.SetActive(false);
+	}
+}
