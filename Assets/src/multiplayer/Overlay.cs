691330c87421925dde31196fde46344a0bc30dc3
Adrian <adrianasdfs@gmail.com>
2017-02-27 00:10:11 +0800

    Script component for the overlay in multiplayer and singleplayer scene

---
+++ Assets/src/multiplayer/Overlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overlay : MonoBehaviour {
+
+	public GameObject messageBox;
+
+	public void showMessageBox()
+	{
+		gameObject.SetActive(true);
+		messageBox.GetComponent<Animator>().SetBool("show",true);
+	}
+}
