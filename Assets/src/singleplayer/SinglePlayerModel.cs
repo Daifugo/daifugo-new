0e73bd57e362dfb424957a9f281ddec8e7db257d
Adrian <adrianasdfs@gmail.com>
2017-02-26 15:16:06 +0800

    Model component of in Single Player scene

---
+++ Assets/src/singleplayer/SinglePlayerModel.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+
+public class SinglePlayerModel : MonoBehaviour, SocketConnectionInterface {
+
+	public GameObject transporter;
+	public GameObject controller;
+	
+	private Transporter _tr;
+	private JToken _responseToken = null;
+	
+	
+
+
+	// Use this for initialization
+	void Start () {
+	
+		_tr = transporter.GetComponent<Transporter>();
+		_tr.setSocketDelegate(this);
+		
+		StartCoroutine(parseData());
+	
+	}
+	
+	
+	IEnumerator parseData()
+	{
+
+		while(_responseToken == null)
+			yield return null;
+
+
+		/* Render out the rules item */
+
+		JArray rms = (JArray)_responseToken;
+		var rules = new List<Dictionary<string,string>>();
+
+		foreach (JToken s in rms) 
+		{
+			var rule = s.ToObject<Dictionary<string,string>> ();
+			rules.Add(rule);
+		}
+		
+		SinglePlayerController c = controller.GetComponent<SinglePlayerController>();
+		c.renderRules(rules.ToArray());
+	}
+	
+	
+	public void receiveData(string dt)
+	{
+		JArray resArray = JArray.Parse (dt);
+		JToken response = resArray.First["response"];
+
+		_responseToken = response.SelectToken ("data");
+	}
+
+
+	public void handleError()
+	{
+
+	}
+}
